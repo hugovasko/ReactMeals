b2751ad47ada9abcd26a3469f3ee7d5bd4cba7af
Hugo Vasko <hugovasko1@gmail.com>
2022-07-04 18:02:32 +0300

    Some good work was done today

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = () => {
+    return <section>
+        <MealsSummary/>
+        <AvailableMeals/>
+    </section>
+};
+
+export default Meals;
