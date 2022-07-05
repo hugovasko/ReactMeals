4a34bd3534147e4d1647582d808183367c15f4a7
Hugo Vasko <hugovasko1@gmail.com>
2022-07-05 23:55:43 +0300

    Cart Context were added & some other little changes

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import styles from "./Modal.module.css";
 
 const Backdrop = (props) => {
-  return <div className={styles.backdrop}></div>;
+  return <div className={styles.backdrop} onClick={props.onClick}></div>;
 };
 
 const ModalOverlay = (props) => {
@@ -20,7 +20,7 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(<Backdrop onClick={props.onClick}/>, portalElement)}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
